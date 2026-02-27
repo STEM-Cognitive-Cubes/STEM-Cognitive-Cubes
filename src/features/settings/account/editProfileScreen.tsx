85093b6ae64f538a4e80e674d0a1c4df5042e711
minduJay <mindujay@gmail.com>
2026-02-27 23:54:36 +0530

    edit profile base setup

---
+++ src/features/settings/account/editProfileScreen.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  SafeAreaView,
+  StatusBar,
+  ScrollView,
+  TextInput,
+  Alert,
+} from 'react-native';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+type RootStackParamList = {
+  Account: undefined;
+};
