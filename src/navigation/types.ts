c86a54e74461cced61fcd3b6aa895597cc621d26
minduJay <mindujay@gmail.com>
2026-02-28 11:07:23 +0530

    added missing navigation types

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,4 +1,27 @@
 export type RootStackParamList = {
-  Login: undefined;
-  Signup: undefined;
-};
+    Login: undefined;
+    Signup: undefined;
+    Home: undefined;
+    Settings: undefined;
+    Account: undefined;
+    EditProfile: undefined;
+    ChangePassword: undefined;
+    DeleteAccount: undefined;
+    AppearanceScreen: undefined;
+    NotificationPreferencesScreen: undefined;
+    PrivacyControlsScreen: undefined;
+    DataSharingScreen: undefined;
+    HelpSupportScreen: undefined;
+    CommunityScreen: undefined;
+    EmailScreen: undefined;
+    ChatScreen: undefined;
+    AppFeatures: undefined;
+    OperateScreen: undefined;
+    ProductIntro: undefined;
+    UserGuide: undefined;
+    Logout: undefined;
+    FAQsScreen: undefined;
+    ContactSupportScreen: undefined;
+
+
+  };
