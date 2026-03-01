12dca852173c5750d000bc967b12b14b880f83fa
C.G.C.D.Gunasekara <chamgu24@gmail.com>
2026-03-01 15:48:49 +0530

    update to display statistical data

--- src/components/StatItem.tsx
+++ src/components/StatItem.tsx
@@ -1,2 +1,14 @@
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { StyleSheet, Text, View } from 'react-native';
+
+export default function StatItem({ icon, label, subLabel, color, iconColor }) {
+  return (
+    <View style={styles.container}>
+      <View style={[styles.circle, { backgroundColor: color }]}>
+        <MaterialCommunityIcons name={icon} size={28} color={iconColor} />
+      </View>
+      <Text style={styles.label}>{label}</Text>
+      <Text style={styles.subLabel}>{subLabel}</Text>
+    </View>
+  );
+}
