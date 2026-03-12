1e82fc1e75196ef45ac2749d0b697894bb22f619
minduJay <mindujay@gmail.com>
2026-03-12 08:35:53 +0530

    updated the App.tsx file

--- App.tsx
+++ App.tsx
@@ -19,10 +19,19 @@ import SignupScreen from "./src/features/auth/screens/SignupScreen";
 
 // Settings Screens
 import SettingsScreen from './src/features/settings/settingsScreen';
+import PrivacyControlsScreen from './src/features/settings/privacyScreen';
 import AccountScreen from './src/features/settings/account/accountScreen';
 import EditProfileScreen from './src/features/settings/account/editProfileScreen';
 import ChangePasswordScreen from './src/features/settings/account/changePasswordScreen';
 import DeleteAccountScreen from './src/features/settings/account/deleteAccountScreen';
+import HelpSupportScreen from './src/features/settings/helpAndSupport/helpScreen';
+import CommunityScreen from './src/features/settings/helpAndSupport/community';
+import EmailScreen from './src/features/settings/helpAndSupport/emailSupport';
+import ChatScreen from './src/features/settings/helpAndSupport/liveChat';
+import UserGuideScreen from './src/features/settings/helpAndSupport/userGuide/userGuide';
+import AppFeaturesScreen from './src/features/settings/helpAndSupport/userGuide/appFeatures';
+import OperateScreen from './src/features/settings/helpAndSupport/userGuide/operateScreen';
+import ProductIntroScreen from './src/features/settings/helpAndSupport/userGuide/productIntro';
 
 // Navigation Types
 import type { RootStackParamList } from "./src/navigation/types";
@@ -91,12 +100,21 @@ export default function App() {
                 headerShadowVisible: false,
               }}
             />
-            <Stack.Screen name="Home" component={AppTabs} />
-            <Stack.Screen name="Settings" component={SettingsScreen} />
-            <Stack.Screen name="Account" component={AccountScreen} />
-            <Stack.Screen name="EditProfile" component={EditProfileScreen} />
-            <Stack.Screen name="ChangePassword" component={ChangePasswordScreen} />
-            <Stack.Screen name="DeleteAccount" component={DeleteAccountScreen} />
+                    <Stack.Screen name="Home" component={AppTabs} />
+                        <Stack.Screen name="Settings" component={SettingsScreen} />
+                        <Stack.Screen name="Account" component={AccountScreen} />
+                        <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+                        <Stack.Screen name="ChangePassword" component={ChangePasswordScreen} />
+                        <Stack.Screen name="DeleteAccount" component={DeleteAccountScreen} />*/}
+                        <Stack.Screen name="HelpSupportScreen" component={HelpSupportScreen} />
+                        <Stack.Screen name="CommunityScreen" component={CommunityScreen} />
+                        <Stack.Screen name="EmailScreen" component={EmailScreen} />
+                        <Stack.Screen name="ChatScreen" component={ChatScreen} />
+                        <Stack.Screen name="AppFeatures" component={AppFeaturesScreen} />
+                        <Stack.Screen name="OperateScreen" component={OperateScreen} />
+                        <Stack.Screen name="ProductIntro" component={ProductIntroScreen} />
+                        <Stack.Screen name="UserGuideScreen" component={UserGuideScreen} />
+                        <Stack.Screen name="PrivacyControlsScreen" component={PrivacyControlsScreen} />
           </Stack.Navigator>
         </NavigationContainer>
       </View>
