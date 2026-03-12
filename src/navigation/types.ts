98dff16cfede416b4a93190c442dca911ebf06ff
minduJay <mindujay@gmail.com>
2026-03-12 08:36:46 +0530

    updated the types.ts file

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -7,4 +7,17 @@ export type RootStackParamList = {
   EditProfile: undefined;
   ChangePassword: undefined;
   DeleteAccount: undefined;
-};
+  AppearanceScreen: undefined;
+  NotificationPreferencesScreen: undefined;
+  PrivacyControlsScreen: undefined;
+  HelpSupportScreen: undefined;
+  CommunityScreen: undefined;
+  EmailScreen: undefined;
+  ChatScreen: undefined;
+  AppFeatures: undefined;
+  OperateScreen: undefined;
+  ProductIntro: undefined;
+  UserGuide: undefined;
+  Logout: undefined;
+  ContactSupportScreen: undefined;
+  };
