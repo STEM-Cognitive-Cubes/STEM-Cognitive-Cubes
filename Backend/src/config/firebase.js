fa747332dd56580b16bfbf13a10ccf6b7719a5c0
Oshadee <oshadeewaduge@gmail.com>
2026-03-21 23:28:15 +0530

    feat(backend): initialize firebase admin application

--- Backend/src/config/firebase.js
+++ Backend/src/config/firebase.js
@@ -1,5 +1,10 @@
 const admin = require('firebase-admin');
 
 // Firebase config module
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.applicationDefault()
+  });
+}
 
 module.exports = {};
