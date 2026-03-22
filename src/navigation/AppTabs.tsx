89433986a7b06a3c151568fc70d0f6a958a5a452
sanuthmee1578 <sanuthmee0625@gmail.com>
2026-03-23 01:10:45 +0530

    Fix: Remove unused temp screen from app tabs

--- src/navigation/AppTabs.tsx
+++ src/navigation/AppTabs.tsx
@@ -4,18 +4,9 @@ import HomeScreen from "@/features/home/screens/HomeScreen";
 import HistoryScreen from "@/features/history/screens/HistoryScreen";
 import InsightsScreen from "@/features/insights/InsightsScreen";
 import SettingsScreen from "@/features/settings/settingsScreen";
-import { View, Text } from "react-native";
 import ProfileScreen from "@/features/profile/profile";
 import BlokCTabBar from "@/navigation/components/BlokCTabBar";
 
-// TEMP screens (replace later with your real ones)
-function TempScreen({ title }: { title: string }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 20, fontWeight: "800" }}>{title}</Text>
-    </View>
-  );
-}
 
 const Tab = createBottomTabNavigator();
 
